0c4fb3099688beda7dd0edaea40865862fd9fb35
Brian Lee <brianlee@Brians-MBP.hsd1.ca.comcast.net>
2020-09-23 01:00:14 -0700

    remove begin button

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,9 @@
-var quizPrompt = $(".context")
+$(document).ready(function() {
+    var quizPrompt = $(".context");
+    var beginButton = $(".begin-button");
+    // remove elements from page to begin quiz
+    beginButton.on("click", function (){
+        beginButton.remove();
+    });
+
+});
